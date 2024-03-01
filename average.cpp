7306db4a0b2de3a7ef8364873ad215c0f538be63
fusenattawat <141806694+fusenattawat@users.noreply.github.com>
2024-03-01 11:56:24 +0700

    lab22.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc,char* argv[]){
+    double sum = 0;
+    for (int i = 0;i < argc;i++){
+        sum += atof(argv[i]);
+    }
+    if(argc > 1){
+        cout << "---------------------------------\n";
+        cout <<"Average of " << argc-1 << " numbers = "<<(sum/(argc-1)) << endl ;
+        cout << "---------------------------------";
+        return 0; 
+    }
+    else {
+        cout << "Please input numbers to find average.";
+    }
+}
